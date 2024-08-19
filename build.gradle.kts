59292564580586d3797ad213d01abd1f70fc575d
Egor Bronnikov <bronnikov.40@mail.ru>
2024-08-20 00:31:41 +0300

    Update build options

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
 plugins {
-    id("java")
+    java
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "dev.ebronnikov.typechecker"
@@ -10,10 +13,27 @@ repositories {
 }
 
 dependencies {
+    implementation("org.antlr:antlr4-runtime:4.7.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks {
+    named<ShadowJar>("shadowJar") {
+        archiveFileName.set("stella-type-checker.jar")
+        manifest {
+            attributes(
+                "Main-Class" to "dev.ebronnikov.typechecker.Main"
+            )
+        }
+        mergeServiceFiles()
+    }
+
+    named<Jar>("jar") {
+        enabled = false
+    }
 }
