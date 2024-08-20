511b7cf1c9f10b8d6bf8a26ad6a29ec44ab3dc18
Egor Bronnikov <bronnikov.40@mail.ru>
2024-08-20 21:28:31 +0300

    Update Gradle build settings

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,17 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.gradleup.shadow") version "8.3.0"
 }
 
 group = "dev.ebronnikov.typechecker"
-version = "1.0-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("org.antlr:antlr4-runtime:4.7.2")
+    implementation("org.antlr:antlr4-runtime:4.13.2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -22,18 +20,12 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks {
-    named<ShadowJar>("shadowJar") {
-        archiveFileName.set("stella-type-checker.jar")
-        manifest {
-            attributes(
-                "Main-Class" to "dev.ebronnikov.typechecker.Main"
-            )
-        }
-        mergeServiceFiles()
+tasks.shadowJar {
+    archiveFileName.set("stella-type-checker.jar")
+    manifest {
+        attributes(
+            "Main-Class" to "dev.ebronnikov.typechecker.Main"
+        )
     }
-
-    named<Jar>("jar") {
-        enabled = false
-    }
+    mergeServiceFiles()
 }
